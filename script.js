64a6adc342e8ea6eba321dc2ed7dd8803597f49b
lucasjoaovieira <joao.lacerda.vieira@escola.pr.gov.br>
2025-03-21 14:08:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let audios = [
     {caminho:'musica4.mp3', legenda:'Hayit'},
     {caminho:'musica5.mp3', legenda:'Helena'},
     {caminho:'musica6.mp3', legenda:'Paula Fernandes'},
-    {caminho:'Paula Fernandes.mp3', legenda:'Paula Fernandes'},
+    {caminho:'musica7.mp3', legenda:'Paula Fernandes'},
     {caminho:'Paula Fernandes.mp3', legenda:'Paula Fernandes'},
     {caminho:'titanic.mp3', legenda:'Titanic'},
 ];
